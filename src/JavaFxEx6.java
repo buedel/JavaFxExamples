8aa9430c81fab169197e29f8cc61a1240229eb63
buedel <buedel@gmail.com>
2020-04-15 21:16:36 -0400

    ListView

--- src/JavaFxEx6.java
+++ src/JavaFxEx6.java
@@ -1,12 +1,8 @@
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
--- src/JavaFxEx8.java
+++ src/JavaFxEx8.java
@@ -1,9 +1,14 @@
 
+import java.util.Observable;
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,6 +17,8 @@ public class JavaFxEx8 extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+
+	ListView<String> listView;
 
 	@Override
 	public void start(Stage window) throws Exception {
@@ -41,15 +48,34 @@ public class JavaFxEx8 extends Application {
 			if (!comboBox.getItems().contains(comboBox.getValue())) {
 				comboBox.getItems().add(comboBox.getValue());
 			}
+			listView.getItems().add(comboBox.getValue());
 		});
 
+		listView = new ListView<String>();
+		listView.getItems().addAll("Item One", "Item Two", "Item Three");
+
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		VBox layout = new VBox(15);
 
-		layout.getChildren().addAll(choiceBox, comboBox, orderButton);
+		Button printItems = new Button("print Items");
+
+		printItems.setOnAction(e -> printItems());
+
+		layout.getChildren().addAll(choiceBox, comboBox, listView, orderButton, printItems);
 
 		Scene scene = new Scene(layout);
 		window.setScene(scene);
 		window.show();
 	}
 
+	private void printItems() {
+		ObservableList<String> movies;
+
+		movies = listView.getSelectionModel().getSelectedItems();
+
+		System.out.println("Selected Items:");
+		for (String msg : movies) {
+			System.out.println("   " + msg);
+		}
+	}
 }
